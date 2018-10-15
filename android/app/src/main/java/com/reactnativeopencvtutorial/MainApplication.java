f1916a2acdd431d5b2320c09a1c88961a3f8e0f9
Ricardo Sanchez <ricardo.sanchez@wrethink.com>
2018-10-15 12:23:14 -0700

    Minor changes for run with the library openCV

--- android/app/src/main/java/com/reactnativeopencvtutorial/MainApplication.java
+++ android/app/src/main/java/com/reactnativeopencvtutorial/MainApplication.java
@@ -82,7 +82,7 @@ public class MainApplication extends Application implements ReactApplication {
   {
     if (!OpenCVLoader.initDebug()) {
       Log.d("OpenCV", "Internal OpenCV library not found. Using OpenCV Manager for initialization");
-      OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION_3_0_0, this, mLoaderCallback);
+      OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION, this, mLoaderCallback);
     } else {
       Log.d("OpenCV", "OpenCV library found inside package. Using it!");
       mLoaderCallback.onManagerConnected(LoaderCallbackInterface.SUCCESS);
